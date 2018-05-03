1a0e2a7b187183b282d861f3af77ddf8a2d35ec5
wkimak <wkimak94@gmail.com>
2018-05-03 12:44:12 -0400

    Added Navbar Component

--- client/src/components/AdminView/AdminDashboardView.jsx
+++ client/src/components/AdminView/AdminDashboardView.jsx
@@ -4,11 +4,15 @@ import { connect } from "react-redux";
 const AdminDashboard = (props) => {
 
   return (
-    <div></div>
+    <div className='company_dashboard'>
+      <div className='initial_challenge'></div>
+      <button>Submit Initial Challenge</button>
+      <div className='company_calendar'></div>
+    </div>
   )
 }
 
-const AdminDashBoardView = connect(mapStateToProps)(AdminDashboard);
+const AdminDashboardView = connect(mapStateToProps)(AdminDashboard);
 //mapstatetoprops --> pull state from the store
 
-export default AdminDashBoardView;
+export default AdminDashboardView;
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+
+import Navbar from './Navbar.jsx';
 import Login from './Login.jsx';
 import CompanySignup from './CompanySignup.jsx';
 import UserSignup from './UserSignup.jsx';
+
 
 class App extends Component {
 
   render() {
     return (
       <div>
+       <Navbar />
        <Login />
        <CompanySignup />
        <UserSignup />
+       
       </div>
     );
   }
