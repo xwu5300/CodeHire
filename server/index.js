97dcd446281df4c1287f75bcfb9263f2aa21d650
wkimak <wkimak94@gmail.com>
2018-05-03 17:05:14 -0400

    Set up controller files

--- server/index.js
+++ server/index.js
@@ -4,10 +4,15 @@ const axios = require('axios');
 const port = 3000;
 const app = express();
 const path = require('path');
+const routes = require('./routes');
+
+
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.use('/', routes);
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
