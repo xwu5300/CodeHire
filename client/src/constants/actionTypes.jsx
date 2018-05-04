e1742741849db89b81dfd6846449ad0f7b8137cd
wkimak <wkimak94@gmail.com>
2018-05-04 16:39:55 -0400

    Started setting up store and connection between containers/redux

---
+++ client/src/constants/actionTypes.jsx
@@ -0,0 +1,5 @@
+export const saveDefaultChallenge = 'saveDefaultChallenge';
+export const deleteCompanyChallenge = 'deleteCompanyChallenge';
+export const getCompanyChallenges = 'getCompanyChallenges';
+export const getDefaultChallenges = 'getDefaultChallenges';
+export const saveCompanyChallenges = 'saveCompanyChallenges';
