d1d8708459e0f5843801c6cfd6efdbe279c09bbb
Amy San Felipe <amysanfelipe@gmail.com>
2018-05-18 19:22:08 -0400

    split up card components between saved and scheduled

---
+++ client/src/components/AdminView/ChallengeListView/dragndropComponents/ScheduledChallengeCard.jsx
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+
+class ScheduledChallengeCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      duration: '',
+      invalid: false,
+      modalIsOpen: false,
+      selected: null,
+      isExpanded: false,
+      dragged: false
+    }
+
+    this.showCalendar = this.showCalendar.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleDurationChange = this.handleDurationChange.bind(this);
+    this.toggleSelectedOn = this.toggleSelectedOn.bind(this);
+    this.toggleSelectedOff = this.toggleSelectedOff.bind(this);
+    this.expandCard = this.expandCard.bind(this);
+  }
+
+  showCalendar() {
+    $('#calendar').calendar('popup', 'show');
+  }
+
+  toggleSelectedOn(idx) {
+    this.setState({
+      selected: idx
+    })
+  }
+
+  toggleSelectedOff(idx) {
+    this.setState({
+      selected: null
+    })
+  }
+
+  handleClick(challenge, scheduleId) {
+    let date = $('#date').val()
+    console.log(this.state.duration, date)
+    if (this.state.duration === '' || !date) {
+      this.setState({
+        invalid: true
+      })
+    } else {
+      this.setState({
+        invalid: false
+      }, () => {
+        this.props.updateChallengeDate(date, this.state.duration, scheduleId, this.props.close)
+      })
+    }
+  }
+
+  handleDurationChange(event) {
+    this.setState({
+      duration: event.target.value
+    })
+  }
+
+  expandCard() {
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    })
+  }
+ 
+  render() {
+    const tab = {
+      height: '35px',
+      overflow: 'hidden',
+    }
+  
+    const calendarId = this.state.selected === this.props.index ? "calendar" : "inactive";
+    const dateId = this.state.selected === this.props.index ? "date" : "inactive";
+
+    return (
+      <div className='ui fluid green card challenge_card' style={this.state.isExpanded ? null : tab}>
+        <div className='content challenge_content'>
+          
+          <i onClick={ () => this.expandCard() } className="angle down icon expand_icon"></i>
+        
+        { this.state.isExpanded ? 
+          <div>
+            <div><b>Title:</b> {this.props.title}</div>  
+            <div><b>Description:</b> {this.props.instruction}</div>
+            <div><b>Difficulty:</b> {this.props.difficulty}</div>
+          </div>
+        :
+        <div>
+          <span style={{fontSize: '20px', position: 'relative', bottom: '5px'}}> { this.props.title } </span>
+          <span style={{position:'relative', bottom: '5px', float: 'right', marginRight: '50px'}}> <b>difficulty:</b> { this.props.difficulty }</span>
+        </div>
+      }
+          <div> 
+              <div className="ui calendar" id={calendarId} onMouseEnter={() => {this.toggleSelectedOn(this.props.index)}} onMouseLeave={() => {this.toggleSelectedOff(this.props.index)}}>
+                <div className="ui input left icon" onClick={this.showCalendar}>
+                  <i className="calendar icon"></i>
+                  <input name="date" type="text" placeholder="Date/Time" id={dateId}/>
+                </div>
+                <button type='button' onClick={ () => this.handleClick(this.props.challenge, this.props.scheduleId) }>Set Date</button>
+              </div>
+
+              <select className="ui dropdown" name="duration" value={this.state.duration} onChange={(e) => this.handleDurationChange(e) }>
+                <option value="">Duration (minutes)</option>
+                <option value="15">15</option>
+                <option value="30">30</option>
+                <option value="60">60</option>
+                <option value="90">90</option>
+              </select>
+          </div>
+
+    
+          <div className='saved_challenges_btns'>
+
+          <button className="ui icon button" onClick={() => this.props.deleteFromCompanySchedule(this.props.scheduleId, this.props.userId) }>
+            <i className="minus icon"></i>
+          </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ScheduledChallengeCard;
--- client/src/components/AdminView/ChallengeListView/dragndropComponents/ScheduledChallenges.jsx
+++ client/src/components/AdminView/ChallengeListView/dragndropComponents/ScheduledChallenges.jsx
@@ -4,24 +4,11 @@ import React, { Component } from 'react';
 import { ItemTypes } from './Constants';
 import { DropTarget } from 'react-dnd';
 
-import ChallengeCard from './ChallengeCard.jsx';
+import ScheduledChallengeCard from './ScheduledChallengeCard.jsx';
 
 
 
 const target = {
-
-  canDrop(props, monitor) {
-    const challenge = monitor.getItem();
-
-    for(let i = 0; i < props.scheduledChallenges.length; i++) {
-      if(challenge.challengeId === props.scheduledChallenges[i].challenge_id) {
-        return false;
-      }
-    }
-    
-    return true;
-  },
-
 
   drop(props, monitor) {
     let challenge = monitor.getItem();
@@ -56,7 +43,7 @@ class ScheduleChallenges extends Component {
         <h1> Scheduled Challenges </h1>
         { this.props.scheduledChallenges ? this.props.scheduledChallenges.map((challenge, i) => {
             return (
-              <ChallengeCard 
+              <ScheduledChallengeCard 
                key={i}
                index={i}
                challenge={ challenge } 
