2d5305eaa3f528e53c8107e0ee745cef194ebffc
Kevin Wang <kevwang76@gmail.com>
2018-05-21 18:42:37 -0400

    merged new tables

--- client/src/components/AdminView/UserResults.jsx
+++ client/src/components/AdminView/UserResults.jsx
@@ -22,7 +22,9 @@ class UserResults extends Component {
   }
 
   handleChange(e) {
-    console.log(e.target.value)
+    this.setState({
+      currentChallenge: e.target.value
+    })
   }
 
   successRate(arr, params, category) {
@@ -50,79 +52,117 @@ class UserResults extends Component {
 
   render() {
     console.log('props on render', this.props)
-    // let IndustrySuccess = this.successRate(this.props.all_results, categories, "category")
-    // let CompanySuccess = this.successRate(this.props.results, categories, "category")
-    //
-    // let Industry = this.props.all_results.map((data) => {
-    //   return {x: data.category, y: Math.round(IndustrySuccess[data.category]* 100) }
-    // }).sort((function(a,b) {
-    //   if (a.x > b.x) {
-    //     return 1
-    //   } else {
-    //     return -1
-    //   }
-    // }))
-    //
-    // let Company = this.props.results.map((data) => {
-    //   return {x: data.category, y: CompanySuccess[data.category] * 100}
-    // }).sort((function(a,b) {
-    //   if (a.x > b.x) {
-    //     return 1
-    //   } else {
-    //     return -1
-    //   }
-    // }))
+    let IndustrySuccess = this.successRate(this.props.all_results, categories, "category")
+    let CompanySuccess = this.successRate(this.props.results, categories, "category")
 
-    let DifficultySuccess = this.successRate(this.props.results, difficulty, "difficulty")
-
-    let Difficulty = this.props.results.map((data)=> {
-      return {x: data.difficulty, y: DifficultySuccess[data.difficulty] * 100}
+    let Industry = this.props.all_results.map((data) => {
+      return {x: data.category, y: Math.round(IndustrySuccess[data.category]* 100) }
     }).sort((function(a,b) {
       if (a.x > b.x) {
-         return 1
-       } else {
-         return -1
-       }
+        return 1
+      } else {
+        return -1
+      }
     }))
-    console.log(Difficulty)
+
+    let Company = this.props.results.map((data) => {
+      return {x: data.category, y: Math.round(CompanySuccess[data.category] * 100)}
+    }).sort((function(a,b) {
+      if (a.x > b.x) {
+        return 1
+      } else {
+        return -1
+      }
+    }))
+
+    //
+    // let DifficultySuccess = this.successRate(this.props.results, difficulty, "difficulty")
+    //
+    // let Difficulty = this.props.results.map((data)=> {
+    //   return {x: data.difficulty, y: DifficultySuccess[data.difficulty] * 100}
+    // }).sort((function(a,b) {
+    //   if (a.x > b.x) {
+    //      return 1
+    //    } else {
+    //      return -1
+    //    }
+    // }))
+
+    // console.log(Difficulty)
     //grey is industry
     //orange is company
     return (
       <div>
         <div style={{ width: `650px`, margin: `auto`, paddingTop: '20' }}>
-          <VictoryChart theme={VictoryTheme.material} domain={{ x: [0, 5], y: [0, 100] }}>
-            <VictoryLabel
-               text={`Pass Rate Per Difficulty`}
-               verticalAnchor={"end"}
-               x={120}
-               y={30}
+          <VictoryChart domainPadding={50} padding={{ left: 70, right: 20, bottom: 50, top: 50 }}>
+              <VictoryLabel
+                text={`Average Results Per Category vs. Yours`}
+                verticalAnchor={"end"}
+                x={120}
+                y={30}
+              />
+              <VictoryAxis
+                axisLabelComponent={<VictoryLabel dy={9}/>}
+                label={"Category"}
+              />
+             <VictoryAxis
+              tickLabelComponent={<VictoryLabel x={50} />}
+              axisLabelComponent={<VictoryLabel dy={-32} />}
+              tickCount={15}
+              tickValues={[10,20,30,40,50,60,70,80,90,100]}
+              tickFormat={(p) => `${p} %`}
+              dependentAxis
+              fixLabelOverlap={true}
+              label={"Pass Percentage"}
+              style={{
+               tickLabels: {
+                 fontSize: 10,
+                 fill: "black",
+               },
+              }}
             />
-            <VictoryAxis
-              axisLabelComponent={<VictoryLabel dy={20}/>}
-              label={"Difficulty"}
+            <VictoryGroup offset={27}
+              colorScale={["#C0C0C0", "Orange"]}
+              animate={{
+                duration: 500,
+                onLoad: { duration: 500 },
+              }}
+              categories={{
+                x:["Algorithms", "Data Structures",  "Systems Design"]
+              }}
+            >
+            <VictoryBar
+              style={{
+                data: {
+                  width: 25
+                },
+                labels: {
+                  fontSize: 10
+                }
+              }}
+              labels={(d) => `${d.y}%`}
+              data={Industry}
             />
-            <VictoryAxis
-            tickLabelComponent={<VictoryLabel x={50} />}
-            axisLabelComponent={<VictoryLabel dy={-28} />}
-            tickCount={15}
-            tickValues={[10,20,30,40,50,60,70,80,90,100]}
-            tickFormat={(p) => `${p} %`}
-            dependentAxis
-            label={"Pass Percentage"}
-            style={{
-             tickLabels: {
-               fontSize: 10,
-               fill: "black",
-             },
-            }}
-           />
-            <VictoryScatter
-              style={{ data: { fill: "#c43a31" } }}
-              size={4}
-              data={Difficulty}
+            <VictoryBar
+              style={{
+                data: {
+                  width: 25
+                },
+                labels: {
+                  fontSize: 10
+                }
+              }}
+              labels={(d) => `${d.y}%`}
+              data={Company}
             />
+            </VictoryGroup>
           </VictoryChart>
         </div>
+        <select value={this.state.currentChallenge} onChange={ (e)=> this.handleChange(e)} >
+          {this.props.results.map((challengeResult, i) => {
+            return <option key={i}>{challengeResult.title}</option>
+          })}
+        </select>
       </div>
     )
 
@@ -237,8 +277,3 @@ export default UserResults;
 //     </VictoryChart>
 //   </div>
 // </div>
-// <select value={this.state.currentChallenge} onChange={ (e)=> this.handleChange(e)}>
-//   {this.props.results.map((challengeResult, i) => {
-//     return <option key={i}>{challengeResult.title}</option>
-//   })}
-// </select>
