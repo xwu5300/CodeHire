7917d15b896eb8fa2f80037ddd2481253a7e3ba1
Amy San Felipe <amysanfelipe@gmail.com>
2018-05-24 09:21:55 -0400

    style updates

--- client/src/components/AdminView/AnalyticsView/PastResults.jsx
+++ client/src/components/AdminView/AnalyticsView/PastResults.jsx
@@ -55,15 +55,17 @@ class PastResults extends Component {
         <tr>
           <th>Username</th>
           <th>Status</th>
-          <th>Save to Favorites</th>
+          <th></th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
       {this.props.pastResults.map((item) => {
         return (
           <tr key={item.candidate_id}>
-            <td onClick={()=> {this.handleModal(item.candidate_id, item.company_schedule_id)}}>{item.username}</td>
+            <td>{item.username}</td>
             <td style={item.user_passed ? {color: 'green'} : {color: 'red'}} >{item.user_passed ? "Passed" : "Failed"}</td>
+            <td className="code cursor" onClick={()=> {this.handleModal(item.candidate_id, item.company_schedule_id)}}>View Code</td>
             <td><i className={!this.props.favorites.includes(item.id) ? "heart outline icon cursor" : "heart icon cursor"} 
             onClick={!this.props.favorites.includes(item.id) ? ()=>{this.props.save(localStorage.getItem('userId'), item.candidate_id)} : () => this.props.remove(localStorage.getItem('userId'), item.candidate_id)}></i></td>
           </tr>
